fdb054757be524355d4e0c63d92ae9755f5bba29
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2016-12-22 17:43:23 +0100

    Input processor MIDI event listeners.

--- static/js/wh/midi/midi.processor.input.js
+++ static/js/wh/midi/midi.processor.input.js
@@ -10,9 +10,39 @@ window.WH = window.WH || {};
     function createMIDIPortIn(specs, my) {
         var that,
             midiInput = specs.midiInput,
+            
+            addEventListeners = function() {
+                midiInput.addListener('noteon', 'all', function(e) {
+                    my.setOutputData({
+                        timestamp: e.receivedTime,
+                        channel: e.channel,
+                        type: e.type,
+                        pitch: e.note.number,
+                        velocity: e.rawVelocity
+                    });
+                });
+                midiInput.addListener('noteoff', 'all', function(e) {
+                    my.setOutputData({
+                        timestamp: e.receivedTime,
+                        channel: e.channel,
+                        type: e.type,
+                        pitch: e.note.number,
+                        velocity: e.rawVelocity
+                    });
+                });
+                midiInput.addListener('controlchange', 'all', function(e) {
+                    my.setOutputData({
+                        timestamp: e.receivedTime,
+                        channel: e.channel,
+                        type: e.type,
+                        controller: e.controller.number,
+                        value: e.value
+                    });
+                });
+            },
                 
             process = function(start, end) {
-                
+                // the midi input has nothing to process but needs this function
             };
        
         my = my || {};
