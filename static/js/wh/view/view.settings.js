8e06eef98e5bacfad535331048d767ace60b251c
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2016-12-28 11:22:42 +0100

    SettingsView object added.

---
+++ static/js/wh/view/view.settings.js
@@ -0,0 +1,21 @@
+/**
+ * Processor settings view.
+ * @namespace WH
+ */
+
+window.WH = window.WH || {};
+
+(function (ns) {
+    
+    function createSettingsView(specs, my) {
+        var that,
+            processor = specs.processor;
+        
+        that = specs.that || {};
+        
+        return that;
+    };
+
+    ns.createSettingsView = createSettingsView;
+
+})(WH);
