db72301aab0f42fc51a9553134612d2551af6ca8
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-02-22 12:31:41 +0100

    Externalcontrol HTML elements renamed to remote.

--- static/js/wh/view/view.remote.js
+++ static/js/wh/view/view.remote.js
@@ -10,8 +10,8 @@ window.WH = window.WH || {};
     function createRemoteView(specs, my) {
         var that,
             midiRemote = specs.midiRemote,
-            rootEl = document.querySelector('#externalcontrol'),
-            listEl = document.querySelector('.externalcontrol__list'),
+            rootEl = document.querySelector('#remote'),
+            listEl = document.querySelector('.remote__list'),
             
             toggleVisibility = function(isVisible) {
                 rootEl.style.display = isVisible ? 'block' : 'none';
