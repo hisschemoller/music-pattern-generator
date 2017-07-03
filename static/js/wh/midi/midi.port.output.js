fd2752bcc468920ec27ebf49852a29f64774b805
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-07-03 14:15:37 +0200

    Toggle network processor  when MIDI output disconnected or reconnected.

--- static/js/wh/midi/midi.port.output.js
+++ static/js/wh/midi/midi.port.output.js
@@ -12,9 +12,24 @@ window.WH = window.WH || {};
             networkProcessorID,
             
             init = function() {
-                my.midiPort.onstatechange = function(e) {
-                    console.log('input onstatechange', e);
-                };
+                my.midiPort.onstatechange = onPortStateChange;
+            },
+            
+            /**
+             * MIDI device was connected or disconnected.
+             * The first time a MIDI device is connected is handled by the midi module.
+             * This handles disconnected or reconnected ports.
+             * @param {Object} e MIDIConnectionEvent object.
+             */
+            onPortStateChange = function(e) {
+                switch (e.port.state) {
+                    case 'connected':
+                        toggleNetwork(true);
+                        break;
+                    case 'disconnected':
+                        toggleNetwork(false);
+                        break;
+                }
             },
 
             /**
