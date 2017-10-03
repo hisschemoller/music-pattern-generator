c7cac100845acf190884c518cf58dffc2264addd
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-10-03 17:31:55 +0200

    Same function renders left and right column layout.

--- static/js/wh/view/view.app.js
+++ static/js/wh/view/view.app.js
@@ -114,152 +114,67 @@ window.WH = window.WH || {};
             
             renderLayout = function(leftColumn = true, rightColumn = true) {
                 if (leftColumn) {
-                    renderLayoutLeftColumn();
-                    // renderColumnLayout(prefsEl, remoteEl);
+                    renderColumnLayout(prefsEl, remoteEl, false);
                 }
                 if (rightColumn) {
-                    renderLayoutRightColumn();
+                    renderColumnLayout(helpEl, editEl, true);
                 }
             },
             
-            // renderColumnLayout = function(topEl, bottomEl) {
-            //     const totalHeight = panelsEl.clientHeight,
-            //         isTopVisible = topEl.dataset.show == 'true',
-            //         isBtmVisible = btmEl.dataset.show == 'true',
-            //         topViewportEl = topEl.querySelector('.panel__viewport'),
-            //         btmViewportEl = btmEl.querySelector('.panel__viewport'),
-            //         topHeight = topEl.clientHeight,
-            //         btmHeight = btmEl.clientHeight,
-            //         topContentHeight = topEl.querySelector('.panel__content').clientHeight,
-            //         btmContentHeight = btmEl.querySelector('.panel__content').clientHeight;
-            //     
-            //     if (isTopVisible && isBtmVisible) {
-            //         let combinedHeight = topContentHeight + btmContentHeight + (panelHeaderHeight * 2);
-            //         if (combinedHeight > totalHeight) {
-            //             if (topContentHeight + panelHeaderHeight < totalHeight / 2) {
-            //                 topViewportEl.style.height = prefsEl.topContentHeight + 'px';
-            //                 btmViewportEl.style.height = (totalHeight - topContentHeight - (panelHeaderHeight * 2)) + 'px';
-            //             } else if (btmContentHeight + panelHeaderHeight < totalHeight / 2) {
-            //                 topViewportEl.style.height = (totalHeight - btmContentHeight - (panelHeaderHeight * 2)) + 'px';
-            //                 btmViewportEl.style.height = remoteEl.topContentHeight + 'px';
-            //             } else {
-            //                 topViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
-            //                 btmViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
-            //             }
-            //         } else {
-            //             topViewportEl.style.height = 'auto';
-            //             btmViewportEl.style.height = 'auto';
-            //         }
-            //     } else if (isTopVisible) {
-            //         if (topContentHeight + panelHeaderHeight > totalHeight) {
-            //             topViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
-            //         } else {
-            //             topViewportEl.style.height = 'auto';
-            //         }
-            //     } else if (isBtmVisible) {
-            //         if (btmContentHeight + panelHeaderHeight > totalHeight) {
-            //             btmViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
-            //         } else {
-            //             btmViewportEl.style.height = 'auto';
-            //         }
-            //     }
-            // },
-            
-            renderLayoutLeftColumn = function() {
-                const totalHeight = panelsEl.clientHeight,
-                    isPrefsVisible = prefsEl.dataset.show == 'true',
-                    isRemoteVisible = remoteEl.dataset.show == 'true',
-                    prefsViewportEl = prefsEl.querySelector('.panel__viewport'),
-                    remoteViewportEl = remoteEl.querySelector('.panel__viewport'),
-                    prefsHeight = prefsEl.clientHeight,
-                    remoteHeight = remoteEl.clientHeight,
-                    prefsContentHeight = prefsEl.querySelector('.panel__content').clientHeight,
-                    remoteContentHeight = remoteEl.querySelector('.panel__content').clientHeight;
-                
-                if (isPrefsVisible && isRemoteVisible) {
-                    let combinedHeight = prefsContentHeight + remoteContentHeight + (panelHeaderHeight * 2);
-                    if (combinedHeight > totalHeight) {
-                        if (prefsContentHeight + panelHeaderHeight < totalHeight / 2) {
-                            prefsViewportEl.style.height = prefsContentHeight + 'px';
-                            remoteViewportEl.style.height = (totalHeight - prefsContentHeight - (panelHeaderHeight * 2)) + 'px';
-                        } else if (remoteContentHeight + panelHeaderHeight < totalHeight / 2) {
-                            prefsViewportEl.style.height = (totalHeight - remoteContentHeight - (panelHeaderHeight * 2)) + 'px';
-                            remoteViewportEl.style.height = remoteContentHeight + 'px';
-                        } else {
-                            prefsViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
-                            remoteViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
-                        }
-                    } else {
-                        prefsViewportEl.style.height = 'auto';
-                        remoteViewportEl.style.height = 'auto';
-                    }
-                } else if (isPrefsVisible) {
-                    if (prefsContentHeight + panelHeaderHeight > totalHeight) {
-                        prefsViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
-                    } else {
-                        prefsViewportEl.style.height = 'auto';
-                    }
-                } else if (isRemoteVisible) {
-                    if (remoteContentHeight + panelHeaderHeight > totalHeight) {
-                        remoteViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
-                    } else {
-                        remoteViewportEl.style.height = 'auto';
-                    }
-                }
-            },
-            
-            renderLayoutRightColumn = function() {
+            renderColumnLayout = function(topEl, btmEl, isRightColumn) {
                 const totalHeight = panelsEl.clientHeight,
                     columnWidth = document.querySelector('.panels__right').clientWidth,
-                    editWidth = editEl.clientWidth,
-                    helpWidth = helpEl.clientWidth,
-                    isEditVisible = editEl.dataset.show == 'true',
-                    isHelpVisible = helpEl.dataset.show == 'true',
-                    editViewportEl = editEl.querySelector('.panel__viewport'),
-                    helpViewportEl = helpEl.querySelector('.panel__viewport'),
-                    editContentHeight = editEl.querySelector('.panel__content').clientHeight,
-                    helpContentHeight = helpEl.querySelector('.help__nav').clientHeight + helpEl.querySelector('.help__copy').clientHeight;
+                    topWidth = topEl.clientWidth,
+                    btmWidth = btmEl.clientWidth,
+                    isTopVisible = topEl.dataset.show == 'true',
+                    isBtmVisible = btmEl.dataset.show == 'true',
+                    topViewportEl = topEl.querySelector('.panel__viewport'),
+                    btmViewportEl = btmEl.querySelector('.panel__viewport'),
+                    topHeight = topEl.clientHeight,
+                    btmHeight = btmEl.clientHeight,
+                    topContentHeight = topEl.querySelector('.panel__content').clientHeight,
+                    btmContentHeight = btmEl.querySelector('.panel__content').clientHeight;
                 
-                if (editWidth + helpWidth < columnWidth) {
-                    if (editContentHeight + panelHeaderHeight > totalHeight) {
-                        editViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
+                if (isRightColumn && (topWidth + btmWidth < columnWidth)) {
+                    if (topContentHeight + panelHeaderHeight > totalHeight) {
+                        topViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
                     } else {
-                        editViewportEl.style.height = 'auto';
+                        topViewportEl.style.height = 'auto';
                     }
-                    if (helpContentHeight + panelHeaderHeight > totalHeight) {
-                        helpViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
+                    if (btmContentHeight + panelHeaderHeight > totalHeight) {
+                        btmViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
                     } else {
-                        helpViewportEl.style.height = 'auto';
+                        btmViewportEl.style.height = 'auto';
                     }
                 } else {
-                    if (isEditVisible && isHelpVisible) {
-                        let combinedHeight = editContentHeight + helpContentHeight + (panelHeaderHeight * 2);
+                    if (isTopVisible && isBtmVisible) {
+                        let combinedHeight = topContentHeight + btmContentHeight + (panelHeaderHeight * 2);
                         if (combinedHeight > totalHeight) {
-                            if (editContentHeight + panelHeaderHeight < totalHeight / 2) {
-                                editViewportEl.style.height = editContentHeight + 'px';
-                                helpViewportEl.style.height = (totalHeight - editContentHeight - (panelHeaderHeight * 2)) + 'px';
-                            } else if (helpContentHeight + panelHeaderHeight < totalHeight / 2) {
-                                editViewportEl.style.height = (totalHeight - helpContentHeight - (panelHeaderHeight * 2)) + 'px';
-                                helpViewportEl.style.height = helpContentHeight + 'px';
+                            if (topContentHeight + panelHeaderHeight < totalHeight / 2) {
+                                topViewportEl.style.height = prefsEl.topContentHeight + 'px';
+                                btmViewportEl.style.height = (totalHeight - topContentHeight - (panelHeaderHeight * 2)) + 'px';
+                            } else if (btmContentHeight + panelHeaderHeight < totalHeight / 2) {
+                                topViewportEl.style.height = (totalHeight - btmContentHeight - (panelHeaderHeight * 2)) + 'px';
+                                btmViewportEl.style.height = remoteEl.topContentHeight + 'px';
                             } else {
-                                editViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
-                                helpViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
+                                topViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
+                                btmViewportEl.style.height = ((totalHeight / 2) - panelHeaderHeight) + 'px';
                             }
                         } else {
-                            editViewportEl.style.height = 'auto';
-                            helpViewportEl.style.height = 'auto';
+                            topViewportEl.style.height = 'auto';
+                            btmViewportEl.style.height = 'auto';
                         }
-                    } else if (isEditVisible) {
-                        if (editContentHeight + panelHeaderHeight >= totalHeight) {
-                            editViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
+                    } else if (isTopVisible) {
+                        if (topContentHeight + panelHeaderHeight > totalHeight) {
+                            topViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
                         } else {
-                            editViewportEl.style.height = 'auto';
+                            topViewportEl.style.height = 'auto';
                         }
-                    } else if (isHelpVisible) {
-                        if (helpContentHeight + panelHeaderHeight >= totalHeight) {
-                            helpViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
+                    } else if (isBtmVisible) {
+                        if (btmContentHeight + panelHeaderHeight > totalHeight) {
+                            btmViewportEl.style.height = totalHeight - panelHeaderHeight + 'px';
                         } else {
-                            helpViewportEl.style.height = 'auto';
+                            btmViewportEl.style.height = 'auto';
                         }
                     }
                 }
