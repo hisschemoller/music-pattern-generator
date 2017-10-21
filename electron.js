4c012f69b1a2efebd0cae2563dd9fc3a166ac683
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-10-21 18:11:58 +0200

    Add icon files and update Electron settings.

--- electron.js
+++ electron.js
@@ -13,7 +13,11 @@ let mainWindow
 
 function createWindow () {
 // Create the browser window.
-mainWindow = new BrowserWindow({width: 800, height: 600})
+mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'assets/icons/png/128x128.png')
+})
 
 // and load the index.html of the app.
 mainWindow.loadURL(url.format({
