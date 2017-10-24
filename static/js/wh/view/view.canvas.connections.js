1ac6e110f86d2512628ee0ebc602c5f32569862b
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-10-24 18:07:57 +0200

    Connections Canvas view setup.

--- static/js/wh/view/view.canvas.connections.js
+++ static/js/wh/view/view.canvas.connections.js
@@ -8,18 +8,58 @@ window.WH = window.WH || {};
 
     function createCanvasConnectionsView(specs, my) {
         var that,
+            canvas,
+            ctx,
+        
+            init = function() {
+                canvas = document.querySelector('.canvas-connections');
+                ctx = canvas.getContext('2d');
+            },
+            
+            enterConnectMode = function() {
+                // listen for mouse or touch
+                canvas.addEventListener(WH.util.eventType.start, onTouchStart);
+                
+                // show inputs and outputs
+                processors.forEach(function(processor) {
+                    
+                });
+            },
+            
+            exitConnectMode = function() {
+                canvas.removeEventListener(WH.util.eventType.start, onTouchStart);
+            },
             
             /**
              * Enter or leave application connect mode.
              * @param  {Boolean} isEnabled True to enable connect mode.
+             * @param  {Array} processors All processors in the network.
              */
-            toggleConnectMode = function(isEnabled) {
-                console.log(isEnabled);
+            toggleConnectMode = function(isEnabled, processors) {
+                console.log(isEnabled, canvas);
+                // show the canvas
+                canvas.dataset.show = isEnabled;
+                
+                if (isEnabled) {
+                    enterConnectMode();
+                } else {
+                    exitConnectMode();
+                }
+            },
+            
+            /**
+             * Start to drag a connection if an output is the startpoint.
+             * @param  {Object} e Event.
+             */
+            onTouchStart = function(e) {
+                
             };
     
     my = my || {};
     
     that = specs.that || {};
+    
+    init();
     
     that.toggleConnectMode = toggleConnectMode;
     return that;
