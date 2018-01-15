35dc271a2aad13071148786cc4a98cdf168e21bc
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-01-15 15:55:33 +0100

    Webpack added to the project.

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
