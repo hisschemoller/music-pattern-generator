89b93de64cf540380d255bd768318ed24bd8bcb9
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-02-06 12:11:22 +0100

    Add babel to support object spread operator.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: './src/js/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    stats: {
+        colors: true
     },
     devtool: 'eval-source-map'
 };
