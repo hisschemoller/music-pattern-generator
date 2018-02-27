caf7b394bc77427a04575dcdc921e1e4f1998e2f
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-02-27 14:58:22 +0100

    Restore MIDI learn and assigned state of settings after persist or import.

--- src/js/wh/view/setting/base.js
+++ src/js/wh/view/setting/base.js
@@ -7,7 +7,7 @@ import createRemoteSettingView from './remote';
 export default function createBaseSettingView(specs, my) {
     var that,
         
-        init = function() {
+        initialise = function() {
             // find template, add clone to settings panel
             let template = document.querySelector('#template-setting-' + my.data.type);
             let clone = template.content.cloneNode(true);
@@ -17,67 +17,85 @@ export default function createBaseSettingView(specs, my) {
             // show label
             my.el.querySelector('.setting__label-text').innerHTML = my.data.label;
 
-            document.addEventListener(my.store.STATE_CHANGE, (e) => {
-                switch (e.detail.action.type) {
-                    case e.detail.actions.CHANGE_PARAMETER:
-                        if (e.detail.action.processorID === my.processorID && 
-                            e.detail.action.paramKey === my.key) {
-                            my.setValue(e.detail.state.processors.byId[my.processorID].params.byId[my.key].value);
-                        }
-                        break;
-                    
-                    case e.detail.actions.RECREATE_PARAMETER:
-                        if (e.detail.action.processorID === my.processorID && 
-                            e.detail.action.paramKey === my.key) {
-                            my.data = e.detail.state.processors.byId[my.processorID].params.byId[my.key];
-                            my.initData();
-                        }
-                        break;
-                    
-                    case e.detail.actions.TOGGLE_MIDI_LEARN_MODE:
-                        if (my.data.isMidiControllable &&
-                            e.detail.state.selectedID == my.processorID) {
-                            my.changeRemoteState(e.detail.state.learnModeActive ? 'enter' : 'exit');
-                        }
-                        break;
-                    
-                    case e.detail.actions.SELECT_PROCESSOR:
-                    case e.detail.actions.DELETE_PROCESSOR:
-                        if (my.data.isMidiControllable) {
-                            if (e.detail.state.learnModeActive) {
-                                my.changeRemoteState(e.detail.state.selectedID == my.processorID ? 'enter' : 'exit');
-                            }
-                        }
-                        break;
-                    
-                    case e.detail.actions.TOGGLE_MIDI_LEARN_TARGET:
-                        if (my.data.isMidiControllable) {
-                            if (e.detail.state.learnModeActive) {
-                                const isTarget = e.detail.state.learnTargetProcessorID === my.processorID && e.detail.state.learnTargetParameterKey === my.key; 
-                                my.changeRemoteState(isTarget ? 'selected' : 'deselected');
-                            }
-                        }
-                        break;
-                    
-                    case e.detail.actions.ASSIGN_EXTERNAL_CONTROL:
-                        if (my.data.isMidiControllable && 
-                            e.detail.state.learnModeActive && 
-                            e.detail.state.learnTargetProcessorID === my.processorID && 
-                            e.detail.state.learnTargetParameterKey === my.key) {
-                            my.changeRemoteState('assigned');
-                        }
-                        break;
-                    
-                    case e.detail.actions.UNASSIGN_EXTERNAL_CONTROL:
-                        if (my.data.isMidiControllable && 
-                            e.detail.state.learnModeActive && 
-                            e.detail.action.processorID === my.processorID && 
-                            e.detail.action.paramKey === my.key) {
-                            my.changeRemoteState('unassigned');
-                        }
-                        break;
+            // restore learn mode
+            const state = specs.store.getState();
+            if (my.data.isMidiControllable && state.learnModeActive) {
+                my.changeRemoteState('enter');
+                if (my.data.remoteChannel != null && my.data.remoteCC != null) {
+                    my.changeRemoteState('assigned');
+                } else if (state.learnTargetProcessorID === my.processorID && state.learnTargetParameterKey === my.key) {
+                    my.changeRemoteState('selected');
                 }
-            });
+            }
+
+            document.addEventListener(my.store.STATE_CHANGE, handleStateChanges);
+        },
+        
+        terminate = function() {
+            document.removeEventListener(my.store.STATE_CHANGE, handleStateChanges);
+        },
+        
+        handleStateChanges = function(e) {
+            switch (e.detail.action.type) {
+                case e.detail.actions.CHANGE_PARAMETER:
+                    if (e.detail.action.processorID === my.processorID && 
+                        e.detail.action.paramKey === my.key) {
+                        my.setValue(e.detail.state.processors.byId[my.processorID].params.byId[my.key].value);
+                    }
+                    break;
+                
+                case e.detail.actions.RECREATE_PARAMETER:
+                    if (e.detail.action.processorID === my.processorID && 
+                        e.detail.action.paramKey === my.key) {
+                        my.data = e.detail.state.processors.byId[my.processorID].params.byId[my.key];
+                        my.initData();
+                    }
+                    break;
+                
+                case e.detail.actions.TOGGLE_MIDI_LEARN_MODE:
+                    if (my.data.isMidiControllable &&
+                        e.detail.state.selectedID == my.processorID) {
+                        my.changeRemoteState(e.detail.state.learnModeActive ? 'enter' : 'exit');
+                    }
+                    break;
+                
+                case e.detail.actions.SELECT_PROCESSOR:
+                case e.detail.actions.DELETE_PROCESSOR:
+                    if (my.data.isMidiControllable) {
+                        if (e.detail.state.learnModeActive) {
+                            my.changeRemoteState(e.detail.state.selectedID == my.processorID ? 'enter' : 'exit');
+                        }
+                    }
+                    break;
+                
+                case e.detail.actions.TOGGLE_MIDI_LEARN_TARGET:
+                    if (my.data.isMidiControllable) {
+                        if (e.detail.state.learnModeActive) {
+                            const isTarget = e.detail.state.learnTargetProcessorID === my.processorID && e.detail.state.learnTargetParameterKey === my.key; 
+                            my.changeRemoteState(isTarget ? 'selected' : 'deselected');
+                        }
+                    }
+                    break;
+                
+                case e.detail.actions.ASSIGN_EXTERNAL_CONTROL:
+                    if (my.data.isMidiControllable && 
+                        e.detail.state.learnModeActive && 
+                        e.detail.state.learnTargetProcessorID === my.processorID && 
+                        e.detail.state.learnTargetParameterKey === my.key) {
+                        my.changeRemoteState('assigned');
+                    }
+                    break;
+                
+                case e.detail.actions.UNASSIGN_EXTERNAL_CONTROL:
+                    if (my.data.isMidiControllable && 
+                        e.detail.state.learnModeActive && 
+                        e.detail.action.processorID === my.processorID && 
+                        e.detail.action.paramKey === my.key) {
+                        my.changeRemoteState('unassigned');
+                    }
+                    break;
+
+            }
         };
         
     my = my || {};
@@ -92,7 +110,7 @@ export default function createBaseSettingView(specs, my) {
         that = createRemoteSettingView(specs, my);
     }
     
-    init();
+    initialise();
 
     return that;
 }
