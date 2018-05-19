d59879ce4149400d33b9802506dca875fd00beb6
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2018-05-19 17:31:53 +0200

    Open dev tools in Electron app.

--- electron.js
+++ electron.js
@@ -1,15 +1,15 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
 // Create the browser window.
@@ -18,26 +18,26 @@ mainWindow = new BrowserWindow({
     height: 600,
     icon: path.join(__dirname, 'assets/icons/png/128x128.png'),
     webPreferences: {
-        backgroundThrottling: false
-    }
+        backgroundThrottling: false,
+    },
 })
 
 // and load the index.html of the app.
 mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '/build/index.html'),
     protocol: 'file:',
-    slashes: true
+    slashes: true,
 }))
 
 // Open the DevTools.
-// mainWindow.webContents.openDevTools()
+mainWindow.webContents.openDevTools();
 
 // Emitted when the window is closed.
 mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
     })
 
     mainWindow.setMinimizable(false);
@@ -47,27 +47,27 @@ mainWindow.on('closed', function () {
 // https://pracucci.com/electron-slow-background-performances.html
 app.commandLine.appendSwitch('disable-renderer-backgrounding');
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow()
+        createWindow();
     }
-})
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
