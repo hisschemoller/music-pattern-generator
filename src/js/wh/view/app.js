7351f0e84da4c13c7b6cc6c1186f782954c48618
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2019-04-03 18:06:41 +0200

    Refactor app.js

--- src/js/wh/view/app.js
+++ src/js/wh/view/app.js
@@ -5,100 +5,100 @@ import addWindowResize from './windowresize.js';
  * Main application view.
  */
 export default function createAppView(specs, my) {
-    var that,
-        store = specs.store,
-        rootEl = document.querySelector('#app'),
-        panelsEl = document.querySelector('.panels'),
-        libraryEl = document.querySelector('.library'),
-        helpEl = document.querySelector('.help'),
-        prefsEl = document.querySelector('.prefs'),
-        editEl = document.querySelector('.edit'),
-        editContentEl = document.querySelector('.edit .panel__content'),
-        remoteEl = document.querySelector('.remote'),
-        settingsViews = [],
-        panelHeaderHeight,
-        resetKeyCombo = [],
-        controls = {
-            new: {
-                type: 'checkbox',
-                input: document.querySelector('#file-new')    
-            },
-            import: {
-                type: 'checkbox',
-                input: document.querySelector('#file-import')    
-            },
-            export: {
-                type: 'checkbox',
-                input: document.querySelector('#file-export')    
-            },
-            play: {
-                type: 'checkbox',
-                input: document.getElementById('play-check')
-            },
-            bpm: {
-                type: 'number',
-                input: document.getElementById('bpm-number')
-            },
-            library: {
-                type: 'checkbox',
-                input: document.getElementById('library-check')
-            },
-            remote: {
-                type: 'checkbox',
-                input: document.getElementById('learn-check')
-            },
-            prefs: {
-                type: 'checkbox',
-                input: document.getElementById('prefs-check')
-            },
-            edit: {
-                type: 'checkbox',
-                input: document.getElementById('edit-check')
-            },
-            connections: {
-                type: 'checkbox',
-                input: document.getElementById('connections-check')
-            },
-            help: {
-                type: 'checkbox',
-                input: document.getElementById('help-check')
-            }
-        },
+  let that,
+    store = specs.store,
+    rootEl = document.querySelector('#app'),
+    panelsEl = document.querySelector('.panels'),
+    libraryEl = document.querySelector('.library'),
+    helpEl = document.querySelector('.help'),
+    prefsEl = document.querySelector('.prefs'),
+    editEl = document.querySelector('.edit'),
+    editContentEl = document.querySelector('.edit .panel__content'),
+    remoteEl = document.querySelector('.remote'),
+    settingsViews = [],
+    panelHeaderHeight,
+    resetKeyCombo = [],
+    controls = {
+      new: {
+        type: 'checkbox',
+        input: document.querySelector('#file-new')    
+      },
+      import: {
+        type: 'checkbox',
+        input: document.querySelector('#file-import')    
+      },
+      export: {
+        type: 'checkbox',
+        input: document.querySelector('#file-export')    
+      },
+      play: {
+        type: 'checkbox',
+        input: document.getElementById('play-check')
+      },
+      bpm: {
+        type: 'number',
+        input: document.getElementById('bpm-number')
+      },
+      library: {
+        type: 'checkbox',
+        input: document.getElementById('library-check')
+      },
+      remote: {
+        type: 'checkbox',
+        input: document.getElementById('learn-check')
+      },
+      prefs: {
+        type: 'checkbox',
+        input: document.getElementById('prefs-check')
+      },
+      edit: {
+        type: 'checkbox',
+        input: document.getElementById('edit-check')
+      },
+      connections: {
+        type: 'checkbox',
+        input: document.getElementById('connections-check')
+      },
+      help: {
+        type: 'checkbox',
+        input: document.getElementById('help-check')
+      }
+    },
         
-        init = function() {
-            controls.new.input.addEventListener('click', function(e) {
-                store.dispatch(store.getActions().newProject());
-            });
-            controls.import.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().importProject(e.target.files[0]));
-            });
-            controls.export.input.addEventListener('click', function(e) {
-                store.dispatch(store.getActions().exportProject());
-            });
-            controls.play.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().setTransport('toggle'));
-            });
-            controls.bpm.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().setTempo(controls.bpm.input.value));
-            });
-            controls.remote.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().toggleMIDILearnMode());
-            });
-            controls.library.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().togglePanel('library'));
-            });
-            controls.prefs.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().togglePanel('preferences'));
-            });
-            controls.edit.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().togglePanel('settings'));
-            });
-            controls.connections.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().toggleConnectMode());
-            });
-            controls.help.input.addEventListener('change', function(e) {
-                store.dispatch(store.getActions().togglePanel('help'));
-            });
+    init = function() {
+      controls.new.input.addEventListener('click', function(e) {
+        store.dispatch(store.getActions().newProject());
+      });
+      controls.import.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().importProject(e.target.files[0]));
+      });
+      controls.export.input.addEventListener('click', function(e) {
+        store.dispatch(store.getActions().exportProject());
+      });
+      controls.play.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().setTransport('toggle'));
+      });
+      controls.bpm.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().setTempo(controls.bpm.input.value));
+      });
+      controls.remote.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().toggleMIDILearnMode());
+      });
+      controls.library.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().togglePanel('library'));
+      });
+      controls.prefs.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().togglePanel('preferences'));
+      });
+      controls.edit.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().togglePanel('settings'));
+      });
+      controls.connections.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().toggleConnectMode());
+      });
+      controls.help.input.addEventListener('change', function(e) {
+        store.dispatch(store.getActions().togglePanel('help'));
+      });
             
             document.addEventListener('keyup', function(e) {
                 switch (e.keyCode) {
@@ -131,84 +131,85 @@ export default function createAppView(specs, my) {
                 }
             });
 
-            document.addEventListener(store.STATE_CHANGE, e => {
-                switch (e.detail.action.type) {
-                    
-                    case e.detail.actions.CREATE_PROJECT:
-                        setProject(e.detail.state);
-                        showPanels(e.detail.state);
-                        controls.bpm.input.value = e.detail.state.bpm;
-                        break;
-                    
-                    case e.detail.actions.ADD_PROCESSOR:
-                        createSettingsViews(e.detail.state);
-                        renderLayout();
-                        break;
-                    
-                    case e.detail.actions.DELETE_PROCESSOR:
-                        deleteSettingsView(e.detail.action.id);
-                        showPanels(e.detail.state);
-                        selectSettingsView(e.detail.state.selectedID);
-                        renderLayout();
-                        break;
+          document.addEventListener(store.STATE_CHANGE, e => {
+            const { action, actions, state } = e.detail;
+            switch (action.type) {
+              
+              case actions.CREATE_PROJECT:
+                setProject(state);
+                showPanels(state);
+                controls.bpm.input.value = state.bpm;
+                break;
+              
+              case actions.ADD_PROCESSOR:
+                createSettingsViews(state);
+                renderLayout();
+                break;
+              
+              case actions.DELETE_PROCESSOR:
+                deleteSettingsView(action.id);
+                showPanels(state);
+                selectSettingsView(state.selectedID);
+                renderLayout();
+                break;
 
-                    case e.detail.actions.SET_TRANSPORT:
-                        controls.play.input.checked = e.detail.state.transport === 'play';
-                        break;
+              case actions.SET_TRANSPORT:
+                controls.play.input.checked = state.transport === 'play';
+                break;
 
-                    case e.detail.actions.SET_TEMPO:
-                        controls.bpm.input.value = e.detail.state.bpm;
-                        break;
-                    
-                    case e.detail.actions.SELECT_PROCESSOR:
-                        selectSettingsView(e.detail.action.id);
-                        // fall through intentional
-                    case e.detail.actions.TOGGLE_MIDI_LEARN_MODE:
-                    case e.detail.actions.TOGGLE_PANEL:
-                        showPanels(e.detail.state);
-                        break;
-                }
-            });
-            
-            // get panel header height from CSS.
-            var style = getComputedStyle(document.body);
-            panelHeaderHeight = parseInt(style.getPropertyValue('--header-height'), 10);
-            
-            my.addWindowResizeCallback(renderLayout);
-            renderLayout();
+              case actions.SET_TEMPO:
+                controls.bpm.input.value = state.bpm;
+                break;
+              
+              case actions.SELECT_PROCESSOR:
+                selectSettingsView(action.id);
+                // fallthrough intentional
+              case actions.TOGGLE_MIDI_LEARN_MODE:
+              case actions.TOGGLE_PANEL:
+                showPanels(state);
+                break;
+            }
+          });
+          
+          // get panel header height from CSS.
+          var style = getComputedStyle(document.body);
+          panelHeaderHeight = parseInt(style.getPropertyValue('--header-height'), 10);
+          
+          my.addWindowResizeCallback(renderLayout);
+          renderLayout();
         },
 
-        /**
-         * Create settings controls view for a processor.
-         * @param  {Object} processor MIDI processor to control with the settings.
-         */
-        createSettingsViews = function(state) {
-            state.processors.allIds.forEach((id, i) => {
-                const processorData = state.processors.byId[id];
-                let exists = false;
-                settingsViews.forEach(settingsView => {
-                    if (settingsView.getID() === id) {
-                        exists = true;
-                    }
-                });
-                if (!exists) {
-                    fetch(`js/wh/processors/${processorData.type}/settings.html`)
-                        .then(
-                            response => response.text(),
-                            error => console.log('An error occurred.', error)
-                        )
-                        .then(html => {
-                            settingsViews.splice(i, 0, createSettingsPanel({
-                                data: processorData,
-                                store: store,
-                                parentEl: editContentEl,
-                                template: html,
-                                isSelected: store.getState().selectedID === processorData.id
-                            }));
-                        });
-                }
-            });
-        },
+  /**
+   * Create settings controls view for a processor.
+   * @param  {Object} processor MIDI processor to control with the settings.
+   */
+  createSettingsViews = function(state) {
+    state.processors.allIds.forEach((id, i) => {
+      const processorData = state.processors.byId[id];
+      let exists = false;
+      settingsViews.forEach(settingsView => {
+        if (settingsView.getID() === id) {
+          exists = true;
+        }
+      });
+      if (!exists) {
+        fetch(`js/wh/processors/${processorData.type}/settings.html`)
+          .then(
+            response => response.text(),
+            error => console.log('An error occurred.', error)
+          )
+          .then(html => {
+            settingsViews.splice(i, 0, createSettingsPanel({
+              data: processorData,
+              store: store,
+              parentEl: editContentEl,
+              template: html,
+              isSelected: store.getState().selectedID === processorData.id
+            }));
+          });
+      }
+    });
+  },
         
   /**
    * Delete settings controls view for a processor.
@@ -232,22 +233,22 @@ export default function createAppView(specs, my) {
     settingsViews.forEach(view => view.select(id));
   },
 
-        setProject = function(state) {
-            var n = settingsViews.length;
-            while (--n >= 0) {
-                deleteSettingsView(settingsViews[n].getID());
-            }
-            createSettingsViews(state);
-        },
-        
-        renderLayout = function(leftColumn = true, rightColumn = true) {
-            if (leftColumn) {
-                renderColumnLayout(prefsEl, remoteEl, false);
-            }
-            if (rightColumn) {
-                renderColumnLayout(helpEl, editEl, true);
-            }
-        },
+  setProject = function(state) {
+    var n = settingsViews.length;
+    while (--n >= 0) {
+      deleteSettingsView(settingsViews[n].getID());
+    }
+    createSettingsViews(state);
+  },
+  
+  renderLayout = function(leftColumn = true, rightColumn = true) {
+    if (leftColumn) {
+      renderColumnLayout(prefsEl, remoteEl, false);
+    }
+    if (rightColumn) {
+      renderColumnLayout(helpEl, editEl, true);
+    }
+  },
         
         renderColumnLayout = function(topEl, btmEl, isRightColumn) {
             const totalHeight = panelsEl.clientHeight,
@@ -315,32 +316,32 @@ export default function createAppView(specs, my) {
             }
         },
         
-        showPanels = function(state) {
-            helpEl.dataset.show = state.showHelpPanel;
-            controls.help.input.checked = state.showHelpPanel;
+    showPanels = function(state) {
+      helpEl.dataset.show = state.showHelpPanel;
+      controls.help.input.checked = state.showHelpPanel;
 
-            prefsEl.dataset.show = state.showPreferencesPanel;
-            controls.prefs.input.checked  = state.showPreferencesPanel;
+      prefsEl.dataset.show = state.showPreferencesPanel;
+      controls.prefs.input.checked  = state.showPreferencesPanel;
 
-            remoteEl.dataset.show = state.learnModeActive;
-            controls.remote.input.checked = state.learnModeActive;
+      remoteEl.dataset.show = state.learnModeActive;
+      controls.remote.input.checked = state.learnModeActive;
 
-            editEl.dataset.show = state.showSettingsPanel;
-            controls.edit.input.checked = state.showSettingsPanel;
+      editEl.dataset.show = state.showSettingsPanel;
+      controls.edit.input.checked = state.showSettingsPanel;
 
-            libraryEl.dataset.show = state.showLibraryPanel;
-            controls.library.input.checked = state.showLibraryPanel;
+      libraryEl.dataset.show = state.showLibraryPanel;
+      controls.library.input.checked = state.showLibraryPanel;
 
-            controls.connections.input.checked = state.connectModeActive;
-            
-            renderLayout();
-        };
+      controls.connections.input.checked = state.connectModeActive;
+      
+      renderLayout();
+    };
     
-    my = my || {};
-    
-    that = addWindowResize(specs, my);
-    
-    init();
-    
-    return that;
+  my = my || {};
+  
+  that = addWindowResize(specs, my);
+  
+  init();
+  
+  return that;
 }
