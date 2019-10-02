60de5a323933a2094a90fa4a9e7edb4d380e3108
Wouter Hisschemöller <email@hisschemoller.com>
2019-10-02 23:43:05 +0200

    Set EPG 3D mesh render order.

--- src/js/wh/processors/epg/object3d.js
+++ src/js/wh/processors/epg/object3d.js
@@ -59,29 +59,37 @@ export function createObject3d(id, inputs, outputs) {
       const hitarea = createCircleFilled(3, defaultColor);
       hitarea.name = 'hitarea';
       hitarea.material.opacity = 0.0;
+      hitarea.renderOrder = 0;
+      
+      const dots = new Group();
+      dots.name = 'dots';
+      dots.renderOrder = 1;
+      
+      const polygon = createPolygon(defaultColor);
+      polygon.name = 'polygon';
+      polygon.renderOrder = 2;
       
       const centreCircle = createCircleOutline(3, defaultColor);
       centreCircle.name = 'centreCircle';
+      centreCircle.renderOrder = 4;
       
       const selectCircle = createCircleOutline(2, defaultColor);
       selectCircle.name = 'select';
+      selectCircle.renderOrder = 5;
       selectCircle.visible = false;
       
       const centreDot = createCircleOutlineFilled(1.5, defaultColor);
       centreDot.name = 'centreDot';
+      centreDot.renderOrder = 6;
       centreDot.visible = false;
       
       const pointer = createShape();
       pointer.name = 'pointer';
-      
-      const polygon = createPolygon(defaultColor);
-      polygon.name = 'polygon';
-      
-      const dots = new Group();
-      dots.name = 'dots';
+      pointer.renderOrder = 7;
 
       const zeroMarker = createCircleOutline(0.5, defaultColor);
       zeroMarker.name = 'zeroMarker';
+      zeroMarker.renderOrder = 8;
 
       const points = [
         new Vector2(0, -1),
@@ -91,22 +99,24 @@ export function createObject3d(id, inputs, outputs) {
       ];
       const rotatedMarker = createShape(points, defaultColor);
       rotatedMarker.name = 'rotatedMarker';
+      rotatedMarker.renderOrder = 8;
 
       const label = new Group();
       label.name = 'label';
       label.scale.set(0.1, 0.1, 1);
       label.translateY(-10);
+      label.renderOrder = 8;
       
       const wheel = new Group();
       wheel.name = 'wheel';
       wheel.userData.id = id;
       wheel.add(hitarea);
+      wheel.add(polygon);
       wheel.add(centreCircle);
       wheel.add(selectCircle);
       wheel.add(centreDot);
-      wheel.add(pointer);
-      wheel.add(polygon);
       wheel.add(dots);
+      wheel.add(pointer);
       wheel.add(zeroMarker);
       wheel.add(rotatedMarker);
       wheel.add(label);
