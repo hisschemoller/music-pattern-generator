3c76862d6d4e7be1d1e52d159635c4aee0384f51
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2019-10-14 15:14:45 +0200

    Network module refactored.

--- src/js/midi/network.js
+++ src/js/midi/network.js
@@ -1,12 +1,112 @@
 import { dispatch, getActions, STATE_CHANGE, } from '../state/store.js';
 import { getProcessorData } from '../core/processor-loader.js';
 
+let processors = [];
+
+/**
+ * Let all processors process their data.
+ * @param {Number} start Start time in ticks of timespan to process.
+ * @param {Number} end End time in ticks of timespan to process.
+ * @param {Number} nowToScanStart Duration from now until start time in ticks.
+ * @param {Number} ticksToMsMultiplier Ticks to ms. conversion multiplier.
+ * @param {Number} offset Position of transport playhead in ticks.
+ * @param {Object} processorEvents Object to collect processor generated events to displayin the view.
+ */
+export function process(start, end, nowToScanStart, ticksToMsMultiplier, offset, processorEvents) {
+	processors.forEach(processor => {
+		processor.process(start, end, nowToScanStart, ticksToMsMultiplier, offset, processorEvents);
+	});
+}
+
 export function setup() {
-    addEventListeners();
+	addEventListeners();
 }
 
 function addEventListeners() {
   document.addEventListener(STATE_CHANGE, handleStateChanges);
+}
+
+/**
+ * Go through all connection data and connect the processors
+ * that are,'t connected yet.
+ * @param {Object} state App state.
+ */
+function connectProcessors(state) {
+	state.connections.allIds.forEach(connectionID => {
+		const connection = state.connections.byId[connectionID];
+		const sourceProcessor = processors.find(processor => processor.getID() === connection.sourceProcessorID);
+		const destinationProcessor = sourceProcessor.getDestinations().find(processor => processor.getID() === connection.destinationProcessorID);
+		if (!destinationProcessor) {
+			sourceProcessor.connect(destinationProcessor);
+		}
+	});
+}
+
+/**
+ * Create a new processor in the network.
+ * @param {Object} state App state.
+ */
+function createProcessors(state) {
+	for (let id of state.processors.allIds) {
+		const processorData = state.processors.byId[id];
+		const isExists = processors.find(processor => processor.getID() === id);
+		if (!isExists) {
+			const module = getProcessorData(processorData.type, 'processor');
+			const processor = module.createProcessor(processorData);
+			processors.push(processor);
+		}
+	};
+	connectProcessors(state);
+	reorderProcessors(state);
+}
+
+/**
+ * Delete processors.
+ * @param {Object} state App state.
+ */
+function deleteProcessors(state) {
+	for (let i = processors.length - 1, n = 0; i >= n; i--) {
+		const processor = processors[i];
+		
+		// search for the processor in the state
+		const processorData = state.processors.allIds.find(processorID => processorID === processor.getID());
+
+		// remove processor if it doesn't exist in the state
+		if (!processorData) {
+			if (processor.terminate instanceof Function) {
+				processor.terminate();
+			}
+			processors.splice(i, 1);
+		}
+	}
+}
+
+/**
+ * Go through all processor outputs and check if 
+ * they still exist in the state. If not, disconnect them.
+ * 
+ * TODO: allow for processors with multiple inputs or outputs.
+ * @param {Object} state App state.
+ */
+function disconnectProcessors(state) {
+	processors.forEach(sourceProcessor => {
+		if (sourceProcessor.getDestinations instanceof Function) {
+			const destinationProcessors = sourceProcessor.getDestinations();
+			destinationProcessors.forEach(destinationProcessor => {
+				let exists = false;
+				state.connections.allIds.forEach(connectionID => {
+					const connection = state.connections.byId[connectionID];
+					if (connection.sourceProcessorID === sourceProcessor.getID() &&
+						connection.destinationProcessorID === destinationProcessor.getID()) {
+						exists = true;
+					}
+				});
+				if (!exists) {
+					sourceProcessor.disconnect(destinationProcessor);
+				}
+			});
+		}
+	});
 }
 
 /**
@@ -16,341 +116,40 @@ function addEventListeners() {
 function handleStateChanges(e) {
   const { state, action, actions, } = e.detail;
   switch (action.type) {
-		case actions.CREATE_PROJECT:
-			disconnectProcessors(state.connections);
-			deleteProcessors(state.processors);
+    case actions.CREATE_PROJECT:
+			disconnectProcessors(state);
+			deleteProcessors(state);
 			createProcessors(state);
 			break;
 
-		case actions.ADD_PROCESSOR:
+    case actions.ADD_PROCESSOR:
 			createProcessors(state);
 			break;
-	
-		case actions.DELETE_PROCESSOR:
-			disconnectProcessors(state.connections);
-			deleteProcessors(state.processors);
-			reorderProcessors(state.processors);
+
+    case actions.DELETE_PROCESSOR:
+			disconnectProcessors(state);
+			deleteProcessors(state);
+			reorderProcessors(state);
 			break;
-	
-		case actions.CONNECT_PROCESSORS:
-			connectProcessors(state.connections);
-			reorderProcessors(state.processors);
+
+    case actions.CONNECT_PROCESSORS:
+			connectProcessors(state);
+			reorderProcessors(state);
 			break;
-	
-		case actions.DISCONNECT_PROCESSORS:
-			disconnectProcessors(state.connections);
-			reorderProcessors(state.processors);
+
+    case actions.DISCONNECT_PROCESSORS:
+			disconnectProcessors(state);
+			reorderProcessors(state);
 			break;
   }
 }
 
 /**
- * Create a new processor in the network.
- * @param {Object} state State processors table.
+ * Reorder the processors according to their order in the state.
+ * @param {Object} state, App state.
  */
-function createProcessors(state) {
-	let loaded = 0;
-	for (let id of state.processors.allIds) {
-		const processorData = state.processors.byId[id];
-		const isExists = processors.find(processor => processor.getID() === id);
-		if (!isExists) {
-			const module = getProcessorData(processorData.type, 'processor');
-			const processor = module.createProcessor({
-				that: {},
-				data: processorData,
-				store,
-			});
-			processors.push(processor);
-			numProcessors = processors.length;
-
-			loaded += 1;
-			if (loaded === state.processors.allIds.length) {
-				connectProcessors(state.connections);
-				reorderProcessors(state.processors);
-			}
-		}
-	};
-}
-
-/**
- * Go through all connection data and create the connections 
- * that don't yet exist.
- */
-function connectProcessors(connections) {
-	connections.allIds.forEach(connectionID => {
-		const connection = connections.byId[connectionID];
-		processors.forEach(sourceProcessor => {
-			if (sourceProcessor.getID() === connection.sourceProcessorID) {
-				let exists = false;
-				sourceProcessor.getDestinations().forEach(destinationProcessor => {
-					if (destinationProcessor.getID() === connection.destinationProcessorID) {
-						exists = true;
-					}
-				});
-				if (!exists) {
-					processors.forEach(destinationProcessor => {
-						if (destinationProcessor.getID() === connection.destinationProcessorID) {
-							sourceProcessor.connect(destinationProcessor);
-						}
-					});
-				}
-			}
-		});
+function reorderProcessors(state) {
+	processors = state.processors.allIds.map(processorId => {
+		return processors.find(processor => processor.getID() === processorId);
 	});
-}
-
-/**
- * Go through all processor outputs and check if 
- * they still exist in the state. If not, disconnect them.
- * 
- * TODO: allow for processors with multiple inputs or outputs.
- */
-function disconnectProcessors(connections) {
-		processors.forEach(sourceProcessor => {
-				if (sourceProcessor.getDestinations instanceof Function) {
-						const destinationProcessors = sourceProcessor.getDestinations();
-						destinationProcessors.forEach(destinationProcessor => {
-								let exists = false;
-								connections.allIds.forEach(connectionID => {
-										const connection = connections.byId[connectionID];
-										if (connection.sourceProcessorID === sourceProcessor.getID() &&
-												connection.destinationProcessorID === destinationProcessor.getID()) {
-												exists = true;
-										}
-								});
-								if (!exists) {
-										sourceProcessor.disconnect(destinationProcessor);
-								}
-						});
-				}
-		});
-},
-
-/**
- * Delete a processor.
- * @param {Object} state State processors table.
- */
-function deleteProcessors(processorsState) {
-	for (let i = processors.length - 1, n = 0; i >= n; i--) {
-
-		// search for the processor in the state
-		let exists = false;
-		processorsState.allIds.forEach(processorID => {
-			if (processorID === processors[i].getID()) {
-				exists = true;
-			}
-		});
-
-		// remove processor if it doesn't exist in the state
-		if (!exists) {
-			const processor = processors[i];
-			if (processor.terminate instanceof Function) {
-				processor.terminate();
-			}
-			processors.splice(i, 1);
-		}
-	}
-	numProcessors = processors.length;
-}
-
-/**
- * Manages the graph of midi processors.
- */
-export default function createMIDINetwork(specs, my) {
-    var that,
-        processors = [],
-        numProcessors = 0,
-
-        init = function() {
-            document.addEventListener(store.STATE_CHANGE, (e) => {
-                const { action, actions, state } = e.detail;
-                switch (action.type) {
-                    case actions.CREATE_PROJECT:
-                        disconnectProcessors(state.connections);
-                        deleteProcessors(state.processors);
-                        createProcessors(state);
-                        break;
-
-                    case actions.ADD_PROCESSOR:
-                        createProcessors(state);
-                        break;
-                    
-                    case actions.DELETE_PROCESSOR:
-                        disconnectProcessors(state.connections);
-                        deleteProcessors(state.processors);
-                        reorderProcessors(state.processors);
-                        break;
-                    
-                    case actions.CONNECT_PROCESSORS:
-                        connectProcessors(state.connections);
-                        reorderProcessors(state.processors);
-                        break;
-                    
-                    case actions.DISCONNECT_PROCESSORS:
-                        disconnectProcessors(state.connections);
-                        reorderProcessors(state.processors);
-                        break;
-                }
-            });
-
-            document.addEventListener('keyup', function(e) {
-                switch (e.keyCode) {
-                    case 83: // s
-                        console.log('    ++++    ');
-                        processors.forEach(processor => {
-                            console.log('network processor', processor.getID());
-                        });
-                        break;
-                }
-            });
-        },
-
-        /**
-         * Create a new processor in the network.
-         * @param {Object} state State processors table.
-         */
-        createProcessors = async state => {
-          let loaded = 0;
-          for (let id of state.processors.allIds) {
-            const processorData = state.processors.byId[id];
-            const isExists = processors.find(processor => processor.getID() === id);
-            if (!isExists) {
-              const module = await import(`../processors/${processorData.type}/processor.js`);
-              const processor = module.createProcessor({
-                that: {},
-                data: processorData,
-                store,
-              });
-              processors.push(processor);
-              numProcessors = processors.length;
-
-              loaded += 1;
-              if (loaded === state.processors.allIds.length) {
-                connectProcessors(state.connections);
-                reorderProcessors(state.processors);
-              }
-            }
-          };
-        },
-
-        /**
-         * Delete a processor.
-         * @param {Object} state State processors table.
-         */
-        deleteProcessors = function(processorsState) {
-            for (let i = processors.length - 1, n = 0; i >= n; i--) {
-                // search for the processor in the state
-                let exists = false;
-                processorsState.allIds.forEach(processorID => {
-                    if (processorID === processors[i].getID()) {
-                        exists = true;
-                    }
-                });
-
-                // remove processor if it doesn't exist in the state
-                if (!exists) {
-                    const processor = processors[i];
-                    if (processor.terminate instanceof Function) {
-                        processor.terminate();
-                    }
-                    processors.splice(i, 1);
-                }
-            }
-            numProcessors = processors.length;
-        },
-        
-        /**
-         * Go through all connection data and create the connections 
-         * that don't yet exist.
-         */
-        connectProcessors = function(connections) {
-            connections.allIds.forEach(connectionID => {
-                const connection = connections.byId[connectionID];
-                processors.forEach(sourceProcessor => {
-                    if (sourceProcessor.getID() === connection.sourceProcessorID) {
-                        let exists = false;
-                        sourceProcessor.getDestinations().forEach(destinationProcessor => {
-                            if (destinationProcessor.getID() === connection.destinationProcessorID) {
-                                exists = true;
-                            }
-                        });
-                        if (!exists) {
-                            processors.forEach(destinationProcessor => {
-                                if (destinationProcessor.getID() === connection.destinationProcessorID) {
-                                    sourceProcessor.connect(destinationProcessor);
-                                }
-                            });
-                        }
-                    }
-                });
-            });
-        },
-
-        /**
-         * Go through all processor outputs and check if 
-         * they still exist in the state. If not, disconnect them.
-         * 
-         * TODO: allow for processors with multiple inputs or outputs.
-         */
-        disconnectProcessors = function(connections) {
-            processors.forEach(sourceProcessor => {
-                if (sourceProcessor.getDestinations instanceof Function) {
-                    const destinationProcessors = sourceProcessor.getDestinations();
-                    destinationProcessors.forEach(destinationProcessor => {
-                        let exists = false;
-                        connections.allIds.forEach(connectionID => {
-                            const connection = connections.byId[connectionID];
-                            if (connection.sourceProcessorID === sourceProcessor.getID() &&
-                                connection.destinationProcessorID === destinationProcessor.getID()) {
-                                exists = true;
-                            }
-                        });
-                        if (!exists) {
-                            sourceProcessor.disconnect(destinationProcessor);
-                        }
-                    });
-                }
-            });
-        },
-
-        /**
-         * Reorder the processors according to their order in the state.
-         * @param {Object} State processor table.
-         */
-        reorderProcessors = function(processorsState) {
-            const orderedProcessors = [];
-            processorsState.allIds.forEach(processorID => {
-                processors.forEach(processor => {
-                    if (processor.getID() === processorID) {
-                        orderedProcessors.push(processor);
-                    }
-                });
-            });
-            processors = orderedProcessors;
-        },
-
-        /**
-         * Let all processors process their data.
-         * @param {Number} start Start time in ticks of timespan to process.
-         * @param {Number} end End time in ticks of timespan to process.
-         * @param {Number} nowToScanStart Duration from now until start time in ticks.
-         * @param {Number} ticksToMsMultiplier Ticks to ms. conversion multiplier.
-         * @param {Number} offset Position of transport playhead in ticks.
-         * @param {Object} processorEvents Object to collect processor generated events to displayin the view.
-         */
-        process = function(start, end, nowToScanStart, ticksToMsMultiplier, offset, processorEvents) {
-            for (var i = 0; i < numProcessors; i++) {
-                processors[i].process(start, end, nowToScanStart, ticksToMsMultiplier, offset, processorEvents);
-            }
-        };
-
-    my = my || {};
-
-    that = specs.that;
-
-    init();
-
-    that.process = process;
-
-    return that;
 }
