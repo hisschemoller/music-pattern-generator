854002d1797a660c95e97caf397efd2f4de22051
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2019-10-15 12:57:38 +0200

    Actions refactored.

--- src/js/core/util.js
+++ src/js/core/util.js
@@ -26,3 +26,51 @@ export function createUUID() {
     return v.toString(16);
   });
 }
+
+/**
+ * Provide a default processor name.
+ * @param {Object} processor Processor to name.
+ * @return {String} Name for a newly created processor.
+ */
+export function getProcessorDefaultName(processors) {
+	let name, number, spaceIndex, 
+		highestNumber = 0,
+		staticName = 'Processor';
+	processors.allIds.forEach(id => {
+		name = processors.byId[id].params.byId.name.value;
+		if (name && name.indexOf(staticName) == 0) {
+			spaceIndex = name.lastIndexOf(' ');
+			if (spaceIndex != -1) {
+				number = parseInt(name.substr(spaceIndex), 10);
+				if (!isNaN(number)) {
+					highestNumber = Math.max(highestNumber, number);
+				}
+			}
+		}
+	});
+	return `${staticName} ${highestNumber + 1}`;
+}
+
+/**
+ * Convert a MIDI control value to a parameter value, depending on the parameter type.
+ * @param {Object} param Processor parameter.
+ * @param {Number} controllerValue MIDI controller value in the range 0 to 127.
+ */
+export function midiControlToParameterValue(param, controllerValue) {
+	const normalizedValue = controllerValue / 127;
+	switch (param.type) {
+		case 'integer':
+			return Math.round(param.min + (param.max - param.min) * normalizedValue);
+		case 'boolean':
+			return normalizedValue > .5;
+		case 'itemized':
+			if (normalizedValue === 1) {
+				return param.model[param.model.length - 1].value;
+			}
+			return param.model[Math.floor(normalizedValue * param.model.length)].value;
+		case 'string':
+		case 'position':
+		default:
+			return param.value;
+	}
+}
