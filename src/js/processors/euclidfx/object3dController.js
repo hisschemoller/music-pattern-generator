5f92559f7f33621e058e1ef99eff096c915bc78b
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2019-10-17 12:24:18 +0200

    Presets fix and styling.

--- src/js/processors/euclidfx/object3dController.js
+++ src/js/processors/euclidfx/object3dController.js
@@ -89,7 +89,7 @@ export function createObject3dController(data, that = {}, my = {}) {
         case actions.LOAD_PRESET:
           const params = state.processors.byId[my.id].params.byId;
           updateDuration(params.steps.value, params.rate.value);
-          updateNecklace(params.steps.value, params.pulses.value, params.rotation.value, params.is_mute.value);
+          updateNecklace(params.steps.value, params.pulses.value, params.rotation.value);
           updateRotation(params.rotation.value);  
           break;
 
